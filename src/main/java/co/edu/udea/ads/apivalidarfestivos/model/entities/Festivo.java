1f11243457b025be735cf2d061a0ad5638e95ba7
DMosqueraL <dml2410@gmail.com>
2023-11-02 07:07:36 -0500

    [FEAT] Se agregan las entidades Festivo y Tipo

--- src/main/java/co/edu/udea/ads/apivalidarfestivos/model/entities/Festivo.java
+++ src/main/java/co/edu/udea/ads/apivalidarfestivos/model/entities/Festivo.java
@@ -1,4 +1,41 @@
 package co.edu.udea.ads.apivalidarfestivos.model.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.Date;
+
+@Data //Getters & Setter de la clase Festivo- Lombok
+@NoArgsConstructor // Constructor vacío de la clase Festivo - Lombok
+@AllArgsConstructor //Constructor con todos los argumentos de la clase Festivo - Lombok
+@Entity
+@Table(name = "festivo")
 public class Festivo {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "seq_festivo")
+    @GenericGenerator(name = "seq_festivo", strategy = "increment")
+    private Integer id;
+
+    @Column(name = "dia")
+    private Integer dia;
+
+    @Column(name = "mes")
+    private Integer mes;
+
+    @Column(name = "nombre", length = 100)
+    private String nombre;
+
+    @Column(name = "diasPascua")
+    private Integer diasPascua;
+
+    private Date fecha;
+
+    @OneToOne
+    @JoinColumn(name = "idTipo", referencedColumnName = "id")
+    private Tipo idTipo;
 }
--- src/main/java/co/edu/udea/ads/apivalidarfestivos/model/entities/Tipo.java
+++ src/main/java/co/edu/udea/ads/apivalidarfestivos/model/entities/Tipo.java
@@ -1,4 +1,22 @@
 package co.edu.udea.ads.apivalidarfestivos.model.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Data //Getters & Setter de la clase Tipo - Lombok
+@NoArgsConstructor // Constructor vacío de la clase Tipo- Lombok
+@AllArgsConstructor //Constructor con todos los argumentos de la clase Tipo- Lombok
+@Entity
 public class Tipo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "seq_tipoFestivo")
+    @GenericGenerator(name = "seq_tipoFestivo", strategy = "increment")
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "idTipo", unique = true, length = 100)
+    private String tipo;
 }
